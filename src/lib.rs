762f5396daa3089735bc969fd17989becaf4ec10
Vera Abramova <abramova.vera@gmail.com>
2023-10-13 23:29:49 +0300

    docs: intro draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! This crate is a shortener and digest generation tool for Substrate chains
+//! metadata.
+//!
+//! During chain data parsing, only small fraction of the chain metadata is
+//! actually utilized.
+//! Hardware signer devices with limited memory capability can encounter
+//! difficulties receiving and processing whole metadata which size is
+//! typically a few hundred kB.
+//! Receiving and using only the part required for decoding of particular data
+//! piece greatly simplifies the task, as the typical metadata part size
+//! decreases down to few kB.
+//!
+//! The decoding of the chain data is beneficial from safety viewpoint only if
+//! the metadata can be guaranteed to not be tampered with.
+//! A possible solution to that would be produce a digest of the metadata and
+//! concat it to the signable transaction prior to signing, so that the
+//! signature would be valid only if the metadata used for decoding matches the
+//! one on chain. This crate generates such digest.
 #![no_std]
 #![deny(unused_crate_dependencies)]
 
